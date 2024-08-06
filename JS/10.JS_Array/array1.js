d0b123fbe0d96b220936b2b4baddb4eb11f1b9fe
y0001ha <yyy96110@naver.com>
2024-08-06 17:27:24 +0900

    20240806 17:27

--- JS/10.JS_Array/array1.js
+++ JS/10.JS_Array/array1.js
@@ -104,74 +104,78 @@ console.log('==============================');
 
 /*
     indexOf(element) : 
-    - 
-    - 
+    - 배열에 매개변수로 전달된 요소가 존재하면 첫번째 인덱스를 반환
+    - 존재하지 않으면 -1을 반환
 */
-
+console.log(fruits.indexOf('apple'));
+console.log(fruits.indexOf('melon'));
 
 console.log('==============================');
 
 /*
     includes(element) :
-    - 
+    - 배열에 전달된 매개변수가 요소에 존재하면 true, 존재하지 않으면 false 반환
 */
-
+console.log(fruits.includes('apple'));
+console.log(fruits.includes('melon'));
 
 console.log('==============================');
 
 /*
-    push(element) :
-    - 
+    push(element) : 
+    - 배열의 마지막 인덱스에 요소를 추가하고 변경된 길이를 반환
 */
-
+console.log(fruits, fruits.length);
+console.log(fruits.push('mango'), fruits);
 
 console.log('==============================');
 
 /*
     pop() :
-    - 
+    - 배열의 마지막 요소를 제거하고 제거한 요소를 반환
 */
-
+console.log(fruits.pop(), fruits);
 
 console.log('==============================');
 
 /*
     shift() :
-    - 
+    - 배열의 첫번째 요소를 제거하고 제거한 요소를 반환
 */
-
+console.log(fruits.shift(), fruits);
 
 console.log('==============================');
 
 /*
     reverse() :
-    - 
+    - 배열의 순서를 역전
 */
-
+console.log(fruits.reverse(), fruits);
 
 console.log('==============================');
 
 /*
     concat(array or item) : 
-    - 
+    - 매개변수로 전달된 값들을 해당 배열의 마지막 요소 뒤에 연결하여 새로운 배열을 반환
 */
-
+var newFruits = fruits.concat(['apple', 'mango']);
+console.log(fruits, newFruits);
 
 console.log('==============================');
 
 /*
     slice(start, end) : 
-    - 
+    - start에 해당하는 인덱스부터 end에 해당하는 인덱스 앞까지 복사하여 반환
 */
-
+console.log(newFruits.slice(1,3), newFruits);
 
 console.log('==============================');
 
 /*
     splice(start, eleteCount, items...) :
-    - 
+    - 배열의 요소를 제거하고 제거한 위치에요소를 갈아끼운 배열을 반환
 */
-
-
+console.log(newFruits.splice(1,2, 'melon'), newFruits);
+console.log(newFruits.splice(2,1), newFruits); //--> 중간에 제거를 하고 싶을때
 
 
