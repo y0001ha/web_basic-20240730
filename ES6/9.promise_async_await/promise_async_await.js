5719697f476fad5ad8b67d2dd8724d1b38d6b2ac
y0001ha <yyy96110@naver.com>
2024-08-08 17:31:00 +0900

    20240808 17:30

--- ES6/9.promise_async_await/promise_async_await.js
+++ ES6/9.promise_async_await/promise_async_await.js
@@ -6,18 +6,41 @@
     - pending : 이행하지 않은 상태, 최초 상태
     - fullfilled : 성공 상태
     - rejected : 실패 상태
-*/
+*/const number = 10;
+const promise = new Promise((resolve, reject) => {
+    if (number % 2 === 0) resolve('Success');
+    else reject('Fail');
+});
 
+// console.log('A Point');
+// console.log(promise);
+// console.log('B Point');
+
+// promise
+//     .then((result) => {console.log(result);})
+//     .catch((result) => {console.log(result);});
+
+// console.log('C Point');
 
 
 console.log('==================================================');
 
 /*
     async - await : 
-    - 
-    - 
-    - 
+    - 비동기 처리를 동기로 구현할 수 있도록 하는 방법
+    - await 키워드로 비동기 처리를 기다리고 작업을  수행
+    - 반드시 async 키워드로 await을 포함하고 있는 함수를 비동기 함수로 변경해야함
 */
+
+// async function asyncFunction () {}
+const asyncFunction = async () => {
+    console.log('A Point');
+    
+    console.log(await promise);
+
+    console.log('B Point');
+}
+asyncFunction();
 
 
 
