6b9d7ce8ea2f70e50c7d8f95c2051517d44076e2
y0001ha <yyy96110@naver.com>
2024-08-08 16:50:55 +0900

    20240808 16:50

--- ES6/4.parameter_handling/parameter_handling.js
+++ ES6/4.parameter_handling/parameter_handling.js
@@ -28,7 +28,7 @@ restFunction2(1, 2, 4, 5, 7, 8);
 console.log('========================================');
 
 /*
-    spread 연산자 : 
+    spread 연산자 :                                         ---@@@@@@@
     - ...을 사용하여 대상의 요소를 하나씩 분리
     - 연산자의 피연산자는 반드시 반복 가능한 객체(배열, 객체, 문자열, ...) 이어야 함
 */
@@ -55,3 +55,40 @@ let newNumbers2 = [ ...numbers, ...numbers2 ];   // => [1, 2, 3, 4, 5 6, 7, 8]
 console.log(newNumbers2);
 newNumbers2 = [ ...numbers2, ...numbers ];  // => [5, 6, 7, 8, 1, 2, 3, 4]
 console.log(newNumbers2);
+
+console.log('========================================');
+
+// 배열 요소 추가에 사용
+newNumbers2 = [ ...newNumbers2, 9, 10 ];
+console.log(newNumbers2);
+
+console.log('========================================');
+
+// 객체 복사, 연결, 요소 추가에 사용
+const king = {
+    name: '이성계',
+    tombName: '태조'
+};
+
+let newKing = { ...king };
+newKing.name = '이방과';
+console.log(king);
+console.log(newKing); // 깊은복사로 새로운 객체 생성
+
+const kingInfo = {
+    address: '서울특별시',
+    country: '고려'
+};
+
+newKing = { ...king, ...kingInfo };
+console.log(newKing);
+newKing = { ...kingInfo , ...king };
+console.log(newKing);
+
+newKing = { ...king, birth: '1335-11-04' };
+console.log(newKing);
+
+newKing = { ...king, name: '이단', birth: '1335-11-04' }; // 동일한 키일경우 바꿔버림
+console.log(newKing);
+newKing = {name: '이단', ...king, birth: '1335-11-04' }; // 순서가 중요함 덮어씌우기 때문에
+console.log(newKing);
--- ES6/6.destructuring/destructuring.js
+++ ES6/6.destructuring/destructuring.js
@@ -1,42 +1,80 @@
-/*
-    디스트럭처링 :
-    - 
-    - 
-*/
+//-@@@@@@@@@@@스프레드랑 디스트럭처링 아주 많이 씀
 
 /*
-    배열 디스트럭처링
-    - 
-    - 
+    디스트럭처링 :    
+    - 구조화된 배열 혹은 객체를 비구조화, 구조화 파괴하여 개별 변수로 할당하는 것
+    - 배열 혹은 객체에서 필요한 데이터만 추출해서 변수로 사용할 수 있도록 함
 */
 
+let fruits = ['apple', 'banana', 'cacao'];
+// const fruit1 = fruits[0];
+// const fruit2 = fruits[1];
+// const fruit3 = fruits[2];
+
+let king = {
+    name: '이성계',
+    tomb: '태조',
+    birth: '1335-11-04'
+};
+// const name = king.name;
+// const tomb = king.tomb;
+// const birth = king.birth;
+
+/*
+    배열 디스트럭처링 [  ]
+    - 배열의 각각의 요소를 배열로부터 추출해서 변수에 할당
+    - 활당 기준은 인덱스
+*/
+
+// fruits = ['apple', 'banana', 'cacao'];
+// 만약에 배열 디스트럭처링에 배열의 길이를 초과한 변수를 선언하면 초과한 범위에 해당하는 변수는 undefined가 지정됨
+const [fruit1, fruit2, fruit3, fruit4] = fruits;
+console.log(fruit1, fruit2, fruit3, fruit4);
 
 
 console.log('========================================');
 
 /*
     spread 연산자를 활용
-    - 
+    - 배열 디스트럭처링 마지막에 스프레드 연산자를 사용하면 남은 모든 요소로 새로운 배열을 생성
 */
+const [anotherFruits, ...othersFruits] = fruits;
+console.log(anotherFruits, othersFruits);
 
+// const [...othersFruits2, anotherFruits2] = fruits;  //순서 중요. 구조파괴
 
 
 console.log('========================================');
 
 /*
-    객체 디스트럭처링
-    - 
-    - 
+    객체 디스트럭처링 {  }
+    - 객체의 각 속성을 객체로부터 추출하여 변수에 할당
+    - 할당 기분은 키
 */
+// king = {
+//     name: '이성계',
+//     tomb: '태조',
+//     birth: '1335-11-04'
+// };
 
+// 객체 디스트럭처링에서는 변수의 순서는 상관없지만 이름은 반드시 객체 키의 이름과 같아야함 
+//const {birth, name, tomb} = king; //순서 상관 없음. 이름같아야함
+//console.log(name, tomb, birth);
+
+// 키 이름과 다르게 사용하고 싶을땐 '키:변수명' 을 사용
+const {name, tomb, birth: birthday} = king; 
+console.log(name, tomb, birthday);
 
 
 console.log('========================================');
 
 /*
     spread 연산자를 활용
-    - 
+    - 객체 디스트럭처링 마지막에 스프레드 연산자를 사용하면 나머지 모든 속성을 묶어서 새로운 객체를 생성
 */
+const { birth, ...reftKing } = king;
+console.log(birth);
+console.log(reftKing);
 
 
 
