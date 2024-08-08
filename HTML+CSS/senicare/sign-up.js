a13b8ea397ee14cc2e8e1c03f126d909470a45ac
y0001ha <yyy96110@naver.com>
2024-08-08 11:55:03 +0900

    20240808 11:50

--- HTML+CSS/senicare/sign-up.js
+++ HTML+CSS/senicare/sign-up.js
@@ -43,9 +43,34 @@ var signUpButtonElement = document.getElementById('sign-up-button');
 var userName = '', userId = '', userPassword = '', userPasswordCheck = '', userTelnumber = '', authNumber = '';
 var isDuplicatedId = true, isPasswordMatch = false, isEqualPassword = false, isSendTel = false, isTelAuth = false;
 
+var isPossible = false;
+
+function checkPossible () {
+    var isAllEnter = userName && userId && userPassword && userPasswordCheck && userTelnumber && authNumber;
+    var isAllCondition = (!isDuplicatedId) && isPasswordMatch && isEqualPassword && isSendTel && isTelAuth;
+    isPossible = isAllEnter && isAllCondition;
+
+    if (isPossible) signUpButtonElement.className = 'button primary full-width';
+    else signUpButtonElement.className = 'button disable full-width';
+}
+
+function userNameInputHandler (event) {
+    userName = event.target.value;
+
+    if (userName) {
+        userNameMessageElement.textContent = '';
+        userNameMessageElement.className = 'message';
+    } else {
+        userNameMessageElement.textContent = '이름을 입력해주세요.';
+        userNameMessageElement.className = 'message error';
+    }
+
+    checkPossible();
+}
+
 function userIdInputHandler (event) {
     isDuplicatedId = true;
-    userId = event.target.value; //---@@@@@@@
+    userId = event.target.value;
 
     userIdMessageElement.textContent = '';
     userIdMessageElement.className = 'message';
@@ -54,10 +79,12 @@ function userIdInputHandler (event) {
     // else userIdButtonElement.className = 'input-button disable';
 
     userIdButtonElement.className = 'input-button ' + (userId ? 'active' : 'disable');
+
+    checkPossible();
 }
 
 function userPasswordInputHandler (event) {
-    var pattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,13}$/; //정규식
+    var pattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,13}$/;
     userPassword = event.target.value;
 
     userPasswordMessageElement.textContent = '';
@@ -69,6 +96,8 @@ function userPasswordInputHandler (event) {
         userPasswordMessageElement.textContent = '영문, 숫자를 혼용하여 8 ~ 13자 입력해주세요.';
         userPasswordMessageElement.className = 'message error';
     }
+
+    checkPossible();
 }
 
 function userPasswordCheckInputHandler (event) {
@@ -83,11 +112,14 @@ function userPasswordCheckInputHandler (event) {
         userPasswordCheckMessageElement.textContent = '비밀번호가 일치하지 않습니다.';
         userPasswordCheckMessageElement.className = 'message error';
     }
+
+    checkPossible();
 }
 
 function userTelnumberInputHandler (event) {
     isSendTel = false;
     authNumberBoxElement.style.display = 'none';
+    authNumberElement.value = '';
 
     var pattern = /^[0-9]{11}$/;
     userTelnumber = event.target.value;
@@ -106,61 +138,74 @@ function userTelnumberInputHandler (event) {
         userTelnumberMessageElement.className = 'message error';
         userTelnumberButtonElement.className = 'input-button disable'; 
     }
+    
+    checkPossible();
 }
 
 function authNumberInputHandler (event) {
     isTelAuth = false;
-    var authNumber = event.target.value;
+    authNumber = event.target.value;
     if (authNumber) authNumberButtonElement.className = 'input-button active';
     else authNumberButtonElement.className = 'input-button disable';
+
+    checkPossible();
 }
 
 function userIdButtonClickHandler (event) {
-    var userId = userIdElement.value;
     if (!userId) return;
 
-    var existedId = users.some(function (item, index) {
+    isDuplicatedId = users.some(function (item, index) {
         return item.id === userId;
     });
     
-    if (existedId) {
+    if (isDuplicatedId) {
         userIdMessageElement.textContent = '이미 존재하는 아이디입니다.';
         userIdMessageElement.className = 'message error';
     } else {
         userIdMessageElement.textContent = '사용 가능한 아이디입니다.';
         userIdMessageElement.className = 'message primary';
     }
+
+    checkPossible();
 }
 
 function userTelnumberButtonClickHandler (event) {
     var pattern = /^[0-9]{11}$/;
-    var userTelnumber = userTelnumberElement.value;
 
     var isMatched = pattern.test(userTelnumber);
     if (!isMatched) return;
 
+    isSendTel = true;
     userTelnumberMessageElement.textContent = '인증번호가 전송되었습니다.';
     userTelnumberMessageElement.className = 'message primary';
     authNumberBoxElement.style.display = 'flex';
+
+    checkPossible();
 }
 
 function authNumberButtonClickHandler () {
-    var authNumber = authNumberElement.value;
     if (!authNumber) return;
 
-    if (authNumber === '2684') {
+    isTelAuth = authNumber === '2684';
+
+    if (isTelAuth) {
         authNumberMessageElement.textContent = '인증번호가 확인되었습니다.';
         authNumberMessageElement.className = 'message primary';
     } else {
         authNumberMessageElement.textContent = '인증번호가 일치하지 않습니다.';
         authNumberMessageElement.className = 'message error';
     }
+
+    checkPossible();
 }
 
 function signUpButtonClickHandler (event) {
-
+    if (!isPossible) return;
+    alert('회원가입!');
+    location.href = './sign-in.html';
 }
 
+userNameElement.addEventListener('input', userNameInputHandler);
 userIdElement.addEventListener('input', userIdInputHandler);
 userPasswordElement.addEventListener('input', userPasswordInputHandler);
 userPasswordCheckElement.addEventListener('input', userPasswordCheckInputHandler);
@@ -170,4 +215,4 @@ authNumberElement.addEventListener('input', authNumberInputHandler);
 userIdButtonElement.addEventListener('click', userIdButtonClickHandler);
 userTelnumberButtonElement.addEventListener('click', userTelnumberButtonClickHandler);
 authNumberButtonElement.addEventListener('click', authNumberButtonClickHandler);
-signUpButtonElement.addEventListener('click', );
+signUpButtonElement.addEventListener('click', signUpButtonClickHandler);
